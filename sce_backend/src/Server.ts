a51dfc3773fd9c23355438c2eec116edbac6d89b
Daniel Vaknin <daniel17vak@gmail.com>
2024-03-31 00:22:04 +0300

    add swagger doc

--- sce_backend/src/Server.ts
+++ sce_backend/src/Server.ts
@@ -1,7 +1,29 @@
 import appInit from "./App";
 const port = process.env.PORT;
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 appInit().then((app) => {
+    if (process.env.NODE_ENV === "development") {
+    const options = {
+      definition: {
+        openapi: "3.0.0",
+        info: {
+          title: "SCE Web Application Backend API",
+          version: "1.0.1",
+          description: "List all the routes of the backend REST API...",
+        },
+        servers: [
+            {
+            url: "http://localhost:" + process.env.PORT,
+          },
+        ],
+      },
+        apis: ["./src/routes/*.ts"],
+    };
+    const specs = swaggerJsDoc(options);
+        app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+    }
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}!`);
     });
